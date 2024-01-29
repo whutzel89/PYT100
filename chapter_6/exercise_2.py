182b55efdff866e21a200cece97ac075e581f81f
William Hutzel <whutzel89@gmail.com>
2024-01-28 22:40:47 -0600

    added exercise_2.py to /chapter_6

---
+++ chapter_6/exercise_2.py
@@ -0,0 +1,10 @@
+def adder(a,b):
+    return a*b
+
+if __name__ == "__main__":
+
+    import sys
+    sys.path.append('C:\\Users\\cwod_admin\\Documents\\PYT100\\chapter_5\\pract_module\\')
+    import importer
+
+    print(adder(5,5))
