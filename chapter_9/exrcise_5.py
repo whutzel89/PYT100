5bb96b4401c79dea79fda1d6ca35fbf79038bf99
Will Hutzel <whutzel89@gmail.com>
2024-02-12 20:20:04 -0800

    final python coding challenge

--- chapter_9/exrcise_5.py
+++ chapter_9/exrcise_5.py
@@ -1,11 +1,31 @@
 class Employee:
-    pass
+    def __init__(self,name,salary,years):
+        self.name = name
+        self.salary = salary
+        self.years = years
+
+    def pension(self):
+        return self.years*self.salary*.1
+
+    def getname(self):
+        return self.name
+
 
 class Manager(Employee):
-    pass
+    def pension(self):
+        return self.years*self.salary*.2
 
-class Executive(manager):
-    pass
+class Executive(Manager):
+    def pension(self):
+        return self.years*self.salary*.3
 
 if __name__ == "__main__":
-    print("test")
+    emp = Employee('bob',10000,10)
+    man = Manager('jim',10000,10)
+    execu = Executive('rick',10000,10)
+    print(emp.getname())
+    print(emp.pension())
+    print(man.getname())
+    print(man.pension())
+    print(execu.getname())
+    print(execu.pension())
